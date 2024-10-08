b423bd87a8a23426db676f03aed49bc10f4b4f66
Shalvi Shukla <shuklashalvi53@gmail.com>
2024-10-09 00:03:19 +0530

    SECOND

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const methodOverride = require("method-override");
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"/public")))
 
 app.engine("ejs" , ejsMate);
 app.use(methodOverride('_method'));
@@ -56,7 +58,12 @@ app.listen(8080 , ()=>{
     res.render("products/new.ejs" , {product});
 });
 
-
+app.get("/products/:id" , async (req,res)=>{
+  let    productId  =  req.params.id ; 
+  const products =  await Product.findById(productId);
+     res.render("products/show.ejs" , {products});
+  console.log(products);
+});
 
 app.post("/products" , async(req,res)=>{
 
@@ -64,43 +71,45 @@ app.post("/products" , async(req,res)=>{
     const newProduct = new Product(req.body.product);
     await newProduct.save() ;
     console.log(req.body.product);
-    // console.log(newProduct);
-    // res.redirect("/products");
+    console.log(newProduct);
+    res.redirect("/products");
     // let product = req.body.product ;
     // console.log(product);
 });
 
-
-app.get("/products/:id" , async (req,res)=>{
-  let    productId  =  req.params.id ; 
-  const products =  await Product.findById(productId);
-     res.render("products/show.ejs" , {products});
-  // console.log(showProduct);
-});
-
-  
-app.put("/products/:id" , async (req,res)=>{
-  let id = req.params.id ;
- const productss = await Product.findByIdAndUpdate(id , {...req.body.product});
-     console.log(productss,"....");
-   res.redirect(`/products/${id}`);
-
-});
-  
- 
-
- app.delete("/produsts/:id" , async(req,res)=>{
-  let id = req.params.id ;
-  const product = await Product.findByIdAndDelete(id);
-  res.redirect("/products");
-  console.log(product);
- }) ;
-
- app.get("/products/:id/edit" , async(req,res)=>{
+   // edit form
+app.get("/products/:id/edit" , async(req,res)=>{
   let id = req.params.id ;
   const product = await Product.findById(id );
   res.render("products/edit.ejs" , {product} );
+
+     
 });
+
+
+  // update edit form
+app.put("/products/:id" , async (req,res)=>{
+  const id = req.params.id;
+    const updateData = {
+      title : req.body.product.title ,
+      description : req.body.product.description,
+      price : req.body.product.price ,
+      sizes : req.body.product.sizes
+
+    }
+   await Product.findByIdAndUpdate(id , updateData, {new : true});
+    res.redirect(`/products/${id}`);
+} );
+ 
+
+ app.delete("/products/:id" , async(req,res)=>{
+  // console.log("Delete request");
+  let id = req.params.id ;
+  const product = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+ }) ;
+
+
 
 
 
