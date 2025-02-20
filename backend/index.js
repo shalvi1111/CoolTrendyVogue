e7d32ef2dfd803b2f6ffbfc2dcf387d80a38c2b7
Shalvi Shukla <shuklashalvi53@gmail.com>
2025-02-20 23:52:31 +0530

    Final Commit

--- backend/index.js
+++ backend/index.js
@@ -66,13 +66,13 @@ app.use(
 // }));
 
 // Middleware to set CORS headers explicitly
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
